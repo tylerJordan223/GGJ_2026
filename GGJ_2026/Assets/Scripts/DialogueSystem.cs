3d95dc69674d3843ee22a7795077fb146e646469
tylerJordan223 <tjlinkmaster22@gmail.com>
2026-01-30 22:22:27 -0500

    Dialogue System Finished

---
+++ GGJ_2026/Assets/Scripts/DialogueSystem.cs
@@ -0,0 +1,245 @@
+using GameInput;
+using Ink.Runtime;
+using System;
+using System.Collections;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+
+public class DialogueSystem : MonoBehaviour
+{
+    //Actual story being printed out
+    public static event Action<Story> OnCreateStory;
+    public Story story;
+
+    //Text input
+    [SerializeField] public TextAsset inkJSON = null;
+
+    //prefabs
+    [SerializeField] GameObject choice = null;
+
+    //Canvas information
+    [SerializeField] VerticalLayoutGroup choices = null;
+    [SerializeField] TextMeshProUGUI dialogue_box;
+
+    //Input system
+    private Global_Input input;
+    private float enter;
+
+    //other variables
+    private bool printing;
+    private bool skip;
+    private bool choosing;
+    private int choice_num;
+
+    private void Awake()
+    {
+        input = new Global_Input();
+        printing = false;
+        skip = false;
+        choosing = false;
+        choice_num = -1;
+    }
+
+    //enables the controls for dialogue
+    private void OnEnable()
+    {
+        //add the controls
+        input.Dialogue.Choose.performed += Click;
+        input.Dialogue.Choice.performed += Choose;
+        input.Dialogue.Choose.Enable();
+
+        //clear the choices and start the story
+        ClearChoices();
+
+        //create new story object using input json
+        story = new Story(inkJSON.text);
+        //make sure it refreshes itself for debug purposes
+        if (OnCreateStory != null) OnCreateStory(story);
+
+        DialogueLoop();
+    }
+
+    //diables the controls for dialogue
+    private void OnDisable()
+    {
+        input.Dialogue.Choose.performed -= Click;
+        input.Dialogue.Choice.performed -= Choose;
+        input.Dialogue.Choose.Disable();
+    }
+
+    private void Update()
+    {
+    }
+
+    //coroutine to actually run dialogue
+    private void DialogueLoop()
+    {
+
+        //goes until it reaches a stopping point (choice/end)
+        if(story.canContinue)
+        {
+            string text = story.Continue();
+            text = text.Trim();
+            StartCoroutine(PrintDialogue(dialogue_box, text));
+        }
+        else if(story.currentChoices.Count > 0) //implies choices
+        {
+            //you are now making a choice
+            choosing = true;
+
+            //go through each choice and have them all start printing
+            for(int i = 0; i < story.currentChoices.Count; i++)
+            {
+                GameObject c = Instantiate(choice);
+                c.transform.SetParent(choices.transform);
+                StartCoroutine(PrintDialogue(c.GetComponent<TextMeshProUGUI>(), story.currentChoices[i].text.Trim()));
+            }
+            StartCoroutine(making_choice());
+        }
+    }
+
+    //coroutine to acutally print the dialogue out
+    private IEnumerator PrintDialogue(TextMeshProUGUI text, string dialogue)
+    {
+        printing = true;
+
+        //clear the text before typing it
+        text.text = "";
+
+        float time_between = 0.1f;
+        //do math if its choices to make sure they all print out at the same time to not mess with dialogue
+        if(story.currentChoices.Count > 0)
+        {
+            //make them all take 1.5 seconds to write out
+            time_between = 1.5f / dialogue.Length;
+        }
+
+        //loop for typing the text
+        for(int i = 0; i < dialogue.Length; i++)
+        {
+            text.text = text.text + dialogue[i];
+            yield return new WaitForSeconds(time_between);
+
+            //if you skip just set the dialogue to max
+            if(skip)
+            {
+                text.text = dialogue;
+                break;
+            }
+        }
+
+        //quick pause
+        yield return new WaitForSeconds(0.1f);
+
+        if(skip)
+        {
+            skip = false;
+        }
+
+        printing = false;
+    }
+
+    //loop for the choices
+    private IEnumerator making_choice()
+    {
+        //wait at first
+        while(printing)
+        {
+            yield return new WaitForSeconds(0.1f);
+        }
+
+        //enable the controls for choosing
+        input.Dialogue.Choice.Enable();
+
+        //select the first choice
+        choice_num = 0;
+        TextMeshProUGUI choice_text = choices.transform.GetChild(choice_num).GetComponent<TextMeshProUGUI>();
+        choice_text.text = "-" + choice_text.text + "-";
+
+        while (choosing)
+        {
+            if(choices.transform.GetChild(choice_num).GetComponent<TextMeshProUGUI>() != choice_text)
+            {
+                choice_text.text = choice_text.text.Substring(1, choice_text.text.Length-2);
+                choice_text = choices.transform.GetChild(choice_num).GetComponent<TextMeshProUGUI>();
+                choice_text.text = "-" + choice_text.text + "-";
+            }
+            yield return new WaitForSeconds(0.01f);
+        }
+        
+        //diable the controls for choosing
+        input.Dialogue.Choice.Disable();
+    }
+
+    //function to destroy all choices before making new ones
+    private void ClearChoices()
+    {
+        foreach(Transform child in choices.transform)
+        {
+            Destroy(child.gameObject);
+        }
+    }
+
+    void Click(InputAction.CallbackContext context)
+    {
+        if (printing)
+        {
+            skip = true;
+        }
+        else
+        {
+            //if theres still content to go through
+            if (story.canContinue || !choosing)
+            {
+                DialogueLoop();
+            }
+            else if(story.currentChoices.Count > 0)
+            {
+                //pick option
+                story.ChooseChoiceIndex(choice_num);
+                choosing = false;
+                ClearChoices();
+                DialogueLoop();
+            }
+        }
+    }
+
+    //swapping between choices when allowed
+    void Choose(InputAction.CallbackContext context)
+    {
+        if(choosing)
+        {
+            //getting input
+            Vector2 menu = input.Dialogue.Choice.ReadValue<Vector2>();
+            //move through the list
+            //up
+            if(menu.y > 0.0f)
+            {
+                if (choice_num == 0)
+                {
+                    choice_num = story.currentChoices.Count - 1;
+                }
+                else
+                {
+                    choice_num -= 1;
+                }
+            }
+            //down
+            else if(menu.y < 0.0f)
+            {
+                if (choice_num == story.currentChoices.Count - 1)
+                {
+                    choice_num = 0;
+                }
+                else
+                {
+                    choice_num += 1;
+                }
+            }
+        }
+    }
+}
--- GGJ_2026/Assets/Scripts/MainCharacter.cs
+++ GGJ_2026/Assets/Scripts/MainCharacter.cs
@@ -1,28 +1,31 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using GameInput;
 
 public class MainCharacter : MonoBehaviour
 {
 
     //player  controls
-    public InputAction controls;
+    private Global_Input gi;
+    private InputAction movement;
     public float moveSpeed;
     private Rigidbody2D rb;
 
-    private void Start()
+    private void Awake()
     {
+        gi = new Global_Input();
         rb = GetComponent<Rigidbody2D>();
     }
 
     private void OnEnable()
     {
         //activate the player controls
-        controls.Enable();
+        gi.Player.Enable();
     }
 
     private void Update()
     {
-        Vector2 movement = controls.ReadValue<Vector2>();
+        Vector2 movement = gi.Player.Move.ReadValue<Vector2>();
 
         rb.linearVelocity = movement * moveSpeed;
     }
