794320c045d4f611768b34e1b4a750a2335f7ab1
tylerJordan223 <tjlinkmaster22@gmail.com>
2026-01-31 15:34:46 -0500

    House fade in/out done

--- GGJ_2026/Assets/Scripts/World/RoomScript.cs
+++ GGJ_2026/Assets/Scripts/World/RoomScript.cs
@@ -7,8 +7,12 @@ public class RoomScript : MonoBehaviour
 
     public GameObject room_cam;
     private BoxCollider2D room_col;
+    private SpriteRenderer cover;
 
-    private RoomHandler parent;
+    public RoomHandler parent;
+
+    private bool active;
+    private float fade;
 
     private void Awake()
     {
@@ -16,15 +20,49 @@ public class RoomScript : MonoBehaviour
 
         room_cam = transform.GetChild(0).gameObject;
         room_cam.SetActive(false);
+
+        cover = transform.GetChild(1).GetComponent<SpriteRenderer>();
+
+        active = false;
+        fade = 1.0f;
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         parent.room_queue.Add(room_cam);
+
+        active = true;
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
         parent.room_queue.Remove(room_cam);
+
+        active = false;
+    }
+
+    private void Update()
+    {
+        if(active && fade > 0)
+        {
+            fade -= 0.01f;
+            cover.color = new Color(cover.color.r, cover.color.g, cover.color.b, fade);
+
+            if(fade < 0)
+            {
+                fade = 0;
+            }
+        }
+
+        if(!active && fade < 1.0f)
+        {
+            fade += 0.01f;
+            cover.color = new Color(cover.color.r, cover.color.g, cover.color.b, fade);
+
+            if(fade > 1.0f)
+            {
+                fade = 1.0f;
+            }
+        }
     }
 }
--- GGJ_2026/Assets/Scripts/World/TeleportScript.cs
+++ GGJ_2026/Assets/Scripts/World/TeleportScript.cs
@@ -1,4 +1,7 @@
+using GameInput;
+using System.Collections;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class TeleportScript : MonoBehaviour
 {
@@ -6,4 +9,78 @@ public class TeleportScript : MonoBehaviour
 
     [SerializeField] Transform Point1;
     [SerializeField] Transform Point2;
+    private GameObject InteractAlert;
+
+    //discern between two points
+    private Transform active_point;
+    private Transform teleport_point;
+
+    //input
+    private Global_Input input;
+
+    private void Awake()
+    {
+        InteractAlert = transform.GetChild(0).gameObject;
+    }
+
+    private void OnEnable()
+    {
+        //initalize new input
+        input = new Global_Input();
+
+        input.Player.Next.performed += StartTeleport;
+        input.Player.Disable();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //get the closer teleporter and work based on that
+        if(collision.CompareTag("Player"))
+        {
+            input.Player.Enable();
+
+            active_point = null;
+
+            if( (Point1.position - MainCharacter.Instance.transform.position).magnitude > (Point2.position - MainCharacter.Instance.transform.position).magnitude )
+            {
+                active_point = Point2;
+                teleport_point = Point1;
+            }
+            else
+            {
+                active_point = Point1;
+                teleport_point = Point2;
+            }
+
+            //enable the notification point on the closer object
+            active_point.GetChild(0).gameObject.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        //disable the notification if leaving
+        active_point.GetChild(0).gameObject.SetActive(false);
+        input.Player.Disable();
+    }
+
+    void StartTeleport(InputAction.CallbackContext context)
+    {
+        input.Player.Disable();
+        //disable player, pause, move players
+        MainCharacter.Instance.DeactivatePlayer();
+        active_point.GetChild(0).gameObject.SetActive(false);
+        StartCoroutine(Teleport());
+    }
+    private IEnumerator Teleport()
+    {
+        //wait a second for the animation
+        yield return new WaitForSeconds(1.0f);
+        //teleport
+        MainCharacter.Instance.transform.position = teleport_point.position;
+        //wait a second
+        yield return new WaitForSeconds(1.0f);
+        //enable player
+        MainCharacter.Instance.ActivatePlayer();
+    }
 }
